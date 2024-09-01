6053ab918f49233b5fc23753edae2e1269c881b8
thevaidik <vaidik50000@gmail.com>
2024-09-01 18:51:31 +0530

    Initial Commit

---
+++ Tinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tinder
+//
+//  Created by Vaidik Dubey on 01/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by Vaidik Dubey on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
